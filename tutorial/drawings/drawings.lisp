d02a78ef7dc5e948704d9aad35a85b7c555b0536
凉凉 <thebigbigwordl@qq.com>
2025-11-19 19:00:52 +0800

    Tutorial: just drawings.

---
+++ tutorial/drawings/drawings.lisp
@@ -0,0 +1,10 @@
+;;;; drawings.lisp --- Just draw something for fun
+
+(uiop:define-package #:mlx-cl.tutorial.drawings
+  (:use :mlx-cl))
+
+(in-package :mlx-cl.tutorial.drawings)
+
+;;; Helper tools
+
+;;;; drawings.lisp ends here
